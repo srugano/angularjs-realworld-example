d065402e4e190345ead1c44d0de8cda6497cb0e7
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2020-02-20 16:39:23 +0100

    Just built an ES6 controller in Angular

---
+++ src/js/auth/auth.config.js
@@ -0,0 +1,23 @@
+function AuthConfig($stateProvider, $httpProvider) {
+  'ngInject';
+
+  // Define the routes
+  $stateProvider
+
+  .state('app.login', {
+    url: '/login',
+    templateUrl: 'auth/auth.html',
+    controller: 'AuthCtrl as $ctrl',
+    title: 'Sign in'
+  })
+
+  .state('app.register', {
+    url: '/register',
+    templateUrl: 'auth/auth.html',
+    controller: 'AuthCtrl as $ctrl',
+    title: 'Sign up'
+  });
+
+};
+
+export default AuthConfig;
